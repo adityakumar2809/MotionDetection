2bf6d7e2592262eed18a60452372ca82ef75512d
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-04-08 00:20:47 +0530

    Add argument accepting function

--- motion_detection.py
+++ motion_detection.py
@@ -7,6 +7,26 @@ import argparse
 from imutils.video import VideoStream
 
 
+def checkForArguments(default_min_area=200):
+    '''Load the arguments present in the module invoking call'''
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        '-v',
+        '--video',
+        help='Path to the video file'
+    )
+    ap.add_argument(
+        '-a',
+        '--min-area',
+        type=int,
+        default=default_min_area,
+        help='Minimum area to be considered a movement'
+    )
+    arguments = vars(ap.parse_args())
+
+    return arguments
+
+
 def main():
     pass
 
